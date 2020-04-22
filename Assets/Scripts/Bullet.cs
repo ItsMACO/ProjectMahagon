62b64b7210dc72297ade98f18167048ad3261391
ItsMACO <macoplays8@gmail.com>
2020-04-22 19:36:53 +0200

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Player player;
+    [SerializeField] float timeToDestroy = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        StartCoroutine(DestroyAfterTime(timeToDestroy));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DestroyAfterTime(float timeUntilObjectDestroyed)
+    {
+        yield return new WaitForSeconds(timeUntilObjectDestroyed);
+        Destroy(gameObject);
+    }
+}
